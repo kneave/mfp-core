0a667b6552ca01ac2401e82194e500234b14682d
Keegan Neave <keegan@neave.engineering>
2022-06-22 18:43:55 +0100

    head also controlled with xbox controller

--- src/head_node.py
+++ src/head_node.py
@@ -9,7 +9,8 @@ from sensor_msgs.msg import Joy
 rb = redboard.RedBoard()
 
 pan = pan_centre = 0
-tilt  = tilt_centre = 0
+tilt = tilt_centre = 0
+roll = 0
 
 pan_min = -0.5 + pan_centre
 pan_max =  0.5 + pan_centre
@@ -35,20 +36,40 @@ def scaleinput(input, invert, scale):
     return scaled
 
 def callback(data):
-    if(data.buttons[6] == 1):
+
+    isXbox = True
+
+    if isXbox == True:
+        control_head = not ((data.buttons[4] == 1) or (data.buttons[5] == 1))
+    else:
+        control_head = data.buttons[2] ==1
+    
+    # 6 is trigger on NE-Thing and View on Xbox
+    reset_head = data.buttons[6] == 1
+
+    if(reset_head):
         print("Resetting head position")
         reset()
     else:
-        if(data.buttons[2] ==1):
+        if(control_head):
             # print(data.axes[3], data.axes[4])
-            pan = scaleinput(data.axes[3], True, 20)
-            tilt = scaleinput(data.axes[4], True, 20)
-            setservos(pan, tilt)
+            pan = scaleinput(data.axes[3], True, 45)
+            tilt = scaleinput(data.axes[4], True, 45)
+
+            if(isXbox == True):
+                # roll will be controlled using A as -ve and B as +ve on Xbox
+                roll = -scaleinput(data.buttons[0], False, 45) + scaleinput(data.buttons[1], False, 45)
+            else:
+                roll = scaleinput(data.axes[5], False, 45
+                )
+            setservos(pan, tilt, roll)
+
         
-def setservos(pan_diff, tilt_diff):
-    global pan, tilt
+def setservos(pan_diff, tilt_diff, roll_diff):
+    global pan, tilt, roll
     pan += pan_diff
     tilt += tilt_diff
+    roll += roll_diff
 
     if(pan > pan_max):
         pan = pan_max
@@ -60,8 +81,8 @@ def setservos(pan_diff, tilt_diff):
     elif(tilt < tilt_min):
         tilt = tilt_min
 
-    rb.s8  =  tilt
-    rb.s9  = -tilt
+    rb.s8  =  tilt + roll
+    rb.s9  = -tilt + roll
     rb.s10 =  pan
     
 def listener():
